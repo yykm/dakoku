10867dfc84ffe17fda496a7d2da7dfe670561c64
yykm <okametuu@gmail.com>
2022-03-26 19:30:18 +0900

    Update: 退勤コマンド改修

--- taikin.sh
+++ taikin.sh
@@ -3,7 +3,9 @@
 cd `dirname $0`
 
 docker-compose up -d
+
+# スリープ挟まないとコネクション確率前にコマンド実行してしまう
 sleep 3
-docker-compose exec app python ./src/dakoku_taikin.py
+docker-compose run -T app bash -c './jpholidayp/jpholidayp || python ./src/dakoku_taikin.py'
 
 docker-compose down
